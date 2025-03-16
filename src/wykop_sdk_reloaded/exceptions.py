81666051bcbc984f5f14cf40e5112f76fff38c16
lukas346 <lukasz.klim92@gmail.com>
2025-03-16 18:35:31 +0100

    Better error handling

--- src/wykop_sdk_reloaded/exceptions.py
+++ src/wykop_sdk_reloaded/exceptions.py
@@ -2,5 +2,26 @@ class WykopApiError(Exception):
     pass
 
 
+class WykopApiAuthorizationError(WykopApiError):
+    """
+    HTTP 403 Brak autoryzacji
+    """
+    pass
+
+
+class WykopApiNotFoundError(WykopApiError):
+    """
+    HTTP 404 Brak Endpointu
+    """
+    pass
+
+
+class WykopApiLimitExceededError(WykopApiError):
+    """
+    HTTP 400 Limit zapytań został przekroczony
+    """
+    pass
+
+
 class AuthError(Exception):
     pass
